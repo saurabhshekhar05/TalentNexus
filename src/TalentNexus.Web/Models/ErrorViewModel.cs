c13226882a70f711cab80dba5ae6cefbf1e01d35
Shekhar <saurabh.c.shekhar@accenture.com>
2026-07-30 03:00:47 +0530

    Initial ASP.NET Core MVC project setup

---
+++ src/TalentNexus.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TalentNexus.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
